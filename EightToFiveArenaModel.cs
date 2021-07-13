142265f9e62027b30e74cc5915cb7a4ad622c44e
OrderWOPower <82205477+OrderWOPower@users.noreply.github.com>
2021-07-13 20:56:24 +0800

    Add files via upload

---
+++ EightToFiveArenaModel.cs
@@ -0,0 +1,43 @@
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.GameComponents.Map;
+using TaleWorlds.Localization;
+
+namespace EightToFiveArena
+{
+    public class EightToFiveArenaModel : DefaultSettlementAccessModel
+    {
+        public override bool CanMainHeroAccessLocation(Settlement settlement, string locationId, out bool disableOption, out TextObject disabledText)
+        {
+            disabledText = TextObject.Empty;
+            disableOption = false;
+            bool result = true;
+            if (locationId == "arena")
+            {
+                result = CanMainHeroGoToArena(out disableOption, out disabledText);
+            }
+            return result;
+        }
+        // If the arena is closed, disable the option for entering the arena and replace the tooltip text.
+        private bool CanMainHeroGoToArena(out bool disableOption, out TextObject disabledText)
+        {
+            if (IsArenaOpen)
+            {
+                disabledText = TextObject.Empty;
+                disableOption = false;
+                return true;
+            }
+            disabledText = new TextObject("Arena is closed before 0800 and after 1700.", null);
+            disableOption = true;
+            return false;
+        }
+        // Change the arena's opening hours.
+        public bool IsArenaOpen
+        {
+            get
+            {
+                int num = CampaignTime.Now.GetHourOfDay;
+                return num >= 8 && num < 17;
+            }
+        }
+    }
+}
---
+++ EightToFiveArenaSubModule.cs
@@ -0,0 +1,19 @@
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace EightToFiveArena
+{
+    // This mod changes the arena's opening hours to 8am - 5pm.
+    public class EightToFiveArenaSubModule : MBSubModuleBase
+    {
+        protected override void OnGameStart(Game game, IGameStarter gameStarter)
+        {
+            if (game.GameType is Campaign)
+            {
+                CampaignGameStarter campaignStarter = (CampaignGameStarter)gameStarter;
+                campaignStarter.AddModel(new EightToFiveArenaModel());
+            }
+        }
+    }
+}
