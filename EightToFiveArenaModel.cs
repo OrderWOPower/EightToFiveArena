f7bf1a162a1d8daa819b90164ec64a12c84c9251
OrderWOPower <82205477+OrderWOPower@users.noreply.github.com>
2022-03-24 19:21:50 +0800

    Updated for Bannerlord 1.7.2.

--- EightToFiveArenaModel.cs
+++ EightToFiveArenaModel.cs
@@ -1,11 +1,22 @@
 ﻿using TaleWorlds.CampaignSystem;
-using TaleWorlds.CampaignSystem.SandBox.GameComponents.Map;
+using TaleWorlds.CampaignSystem.GameComponents;
+using TaleWorlds.CampaignSystem.Settlements;
 using TaleWorlds.Localization;
 
 namespace EightToFiveArena
 {
     public class EightToFiveArenaModel : DefaultSettlementAccessModel
     {
+        // Change the arena's opening hours.
+        public bool IsArenaOpen
+        {
+            get
+            {
+                int num = CampaignTime.Now.GetHourOfDay;
+                return num >= 8 && num < 17;
+            }
+        }
+
         public override bool CanMainHeroAccessLocation(Settlement settlement, string locationId, out bool disableOption, out TextObject disabledText)
         {
             disabledText = TextObject.Empty;
@@ -17,6 +28,7 @@ namespace EightToFiveArena
             }
             return result;
         }
+
         // If the arena is closed, disable the option for entering the arena and replace the tooltip text.
         private bool CanMainHeroGoToArena(out bool disableOption, out TextObject disabledText)
         {
@@ -29,15 +41,6 @@ namespace EightToFiveArena
             disabledText = new TextObject("Arena is closed before 0800 and after 1700.", null);
             disableOption = true;
             return false;
-        }
-        // Change the arena's opening hours.
-        public bool IsArenaOpen
-        {
-            get
-            {
-                int num = CampaignTime.Now.GetHourOfDay;
-                return num >= 8 && num < 17;
-            }
         }
     }
 }
